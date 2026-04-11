f5bdf5a9d28df4ae1d82e4d256e170b3371d57aa
douskinza <douskinza@gmail.com>
2026-04-11 15:03:54 +0100

    Add API call step to Executor class

--- PROJET_DS2/orchestrator/executor.py
+++ PROJET_DS2/orchestrator/executor.py
@@ -1,6 +1,7 @@
 from tools.read_csv_tool import read_csv_file
 from tools.compute_kpis import compute_kpis
 from tools.report import build_report
+from tools.api_tool import fetch_weather
 
 class Executor:
     def __init__(self):
@@ -12,6 +13,8 @@ class Executor:
 
         elif step == "compute_kpis":
             self.data = compute_kpis(self.data)
+        elif step == "api_call":
+            self.data = fetch_weather()
 
         elif step == "report":
             return build_report(self.data)

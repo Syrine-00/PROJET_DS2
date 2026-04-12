e62a2057bb095cd3c80be04e55aa4867631774c8
Syrine-00 <syrine.guermazi12@gmail.com>
2026-04-12 15:34:17 +0100

    Refactor main.py to support multiple scenarios

--- PROJET_DS2/main.py
+++ PROJET_DS2/main.py
@@ -1,7 +1,22 @@
 from orchestrator.run_manager import RunManager
-from workflow.scenario1 import build_task
+from workflow import scenario1, scenario2
+import os
 
-task = build_task()
+print(os.listdir())
+print(os.listdir("data_synthetic"))
+
+choice = input("Choose scenario (1 or 2): ")
+
+if choice == "1":
+    task = scenario1.build_task()
+
+elif choice == "2":
+    task = scenario2.build_task()
+
+else:
+    print("Invalid choice")
+    exit()
+
 result = RunManager().run(task)
 
 print(result)

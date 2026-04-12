81c81ee4588102670199a5ac4bc0d04e278b9687
Syrine-00 <syrine.guermazi12@gmail.com>
2026-04-12 15:32:56 +0100

    Update compute_kpis.py

--- PROJET_DS2/tools/compute_kpis.py
+++ PROJET_DS2/tools/compute_kpis.py
@@ -5,6 +5,9 @@ def compute_kpis(df):
     kpis["total_revenue"] = df["revenue"].sum()
 
     revenue_by_region = df.groupby("region")["revenue"].sum()
-    kpis["top_regions"] = revenue_by_region.sort_values(ascending=False).to_dict()
+
+    kpis["top_regions"] = revenue_by_region.sort_values(
+        ascending=False
+    ).to_dict()
 
     return kpis

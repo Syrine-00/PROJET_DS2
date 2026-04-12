9f3b25c262f56315438dd07e2916721f5b4f8c0d
Syrine-00 <syrine.guermazi12@gmail.com>
2026-04-12 15:29:25 +0100

    Update executor.py

--- PROJET_DS2/orchestrator/executor.py
+++ PROJET_DS2/orchestrator/executor.py
@@ -2,6 +2,7 @@ from tools.read_csv_tool import read_csv_file
 from tools.compute_kpis import compute_kpis
 from tools.report import build_report
 from tools.api_tool import fetch_weather_data
+from tools.api_tool import fetch_api_data
 
 
 class Executor:
@@ -12,15 +13,18 @@ class Executor:
     def execute(self, step, task):
 
         if step == "read_csv":
-            self.data = read_csv_file("data_synthetic/tourism_big.csv")
+            self.data = read_csv_file(task["file"])
 
         elif step == "compute_kpis":
-            self.kpis = compute_kpis(self.data)
+            self.data = compute_kpis(self.data)
+        
+        elif step == "call_api":
+            self.data = fetch_api_data()
 
         elif step == "api_call":
             weather = fetch_weather_data()
 
-            # 🔥 fusion KPI + météo
+            #fusion KPI + météo
             self.data = {
                 "kpis": self.kpis,
                 "weather": weather

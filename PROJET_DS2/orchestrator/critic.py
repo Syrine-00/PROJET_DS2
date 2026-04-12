97e623e16c074fad2ffa11200e8a23c15cc3f780
Syrine-00 <syrine.guermazi12@gmail.com>
2026-04-12 14:41:26 +0100

    Enhance validate method to check for empty data
    Added validation for empty data in Critic class.

--- PROJET_DS2/orchestrator/critic.py
+++ PROJET_DS2/orchestrator/critic.py
@@ -1,9 +1,16 @@
 class Critic:
     def validate(self, result):
+
+        #si none->erreur
         if result is None:
             return False
 
+        #si erreur 
         if isinstance(result, dict) and "error" in result:
+            return False
+
+        #si données vides
+        if isinstance(result, dict) and len(result) == 0:
             return False
 
         return True

9dbadebd1997440501a92e9c57dba827efaeae98
Syrine-00 <syrine.guermazi12@gmail.com>
2026-04-12 15:33:22 +0100

    Update api_tool.py

--- PROJET_DS2/tools/api_tool.py
+++ PROJET_DS2/tools/api_tool.py
@@ -15,3 +15,14 @@ def fetch_weather_data():
         "windspeed": data["current_weather"]["windspeed"],
         "tourism_impact": "high" if data["current_weather"]["temperature"] > 25 else "medium"
     }
+
+def fetch_api_data():
+    return {
+        "top_regions": {
+            "Tunis": 120000,
+            "Sousse": 95000,
+            "Djerba": 87000
+        },
+        "total_revenue": 302000,
+        "average_occupancy_rate": 78
+    }

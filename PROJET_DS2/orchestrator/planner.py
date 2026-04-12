f1072a0f471f5ec02ff2e2cbb93ec35b9e83b74e
Syrine-00 <syrine.guermazi12@gmail.com>
2026-04-12 15:12:11 +0100

    Update planner.py

--- PROJET_DS2/orchestrator/planner.py
+++ PROJET_DS2/orchestrator/planner.py
@@ -1,7 +1,10 @@
 class Planner:
     def plan(self, task):
 
-        if task["type"] == "api":
-            return ["api_call", "report"]
+        if task.get("type") == "csv":
+            return ["read_csv", "compute_kpis", "report"]
 
-        return ["read_csv", "compute_kpis", "report"]
+        elif task.get("type") == "api":
+            return ["call_api", "process_api", "report"]
+
+        return []

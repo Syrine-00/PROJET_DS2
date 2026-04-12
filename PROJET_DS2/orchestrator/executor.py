f3f8b8e8fbd548d939cfcaf8cd2ed299615c8da7
Syrine-00 <syrine.guermazi12@gmail.com>
2026-04-12 14:41:43 +0100

    Enhance Executor to include weather data
    Added weather data to the execution flow and separated KPIs.

--- PROJET_DS2/orchestrator/executor.py
+++ PROJET_DS2/orchestrator/executor.py
@@ -1,11 +1,13 @@
 from tools.read_csv_tool import read_csv_file
 from tools.compute_kpis import compute_kpis
 from tools.report import build_report
-from tools.api_tool import fetch_weather
+from tools.api_tool import fetch_weather_data
+
 
 class Executor:
     def __init__(self):
         self.data = None
+        self.kpis = None
 
     def execute(self, step, task):
 
@@ -13,10 +15,16 @@ class Executor:
             self.data = read_csv_file("data_synthetic/tourism_big.csv")
 
         elif step == "compute_kpis":
-            self.data = compute_kpis(self.data)
+            self.kpis = compute_kpis(self.data)
 
         elif step == "api_call":
-            self.data = fetch_weather()
+            weather = fetch_weather_data()
+
+            # 🔥 fusion KPI + météo
+            self.data = {
+                "kpis": self.kpis,
+                "weather": weather
+            }
 
         elif step == "report":
             return build_report(self.data)
